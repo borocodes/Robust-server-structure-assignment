0c031782b38107d99dbeaff45e93139bc11898e7
Jonathan Borofsky <jonborofsky@gmail.com>
2021-10-03 18:25:25 -0500

    initial app structure setup

--- src/app.js
+++ src/app.js
@@ -7,3 +7,22 @@ app.use(express.json());
 // TODO: Add code to meet the requirements and make the tests pass.
 
 module.exports = app;
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+
+
+// TODO: Add code to meet the requirements and make the tests pass.
+//Routes
+
+
+//NotFound
+
+
+//Error handler
+
+
+module.exports = app;
